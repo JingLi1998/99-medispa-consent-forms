fe075e46e116b8f00b80581f5e42f9cbde2c8bba
Jing Li <z5112692@ad.unsw.edu.au>
2021-03-07 19:17:43 +1100

    MS99-9: Hook up state actions and getters

--- src/components/CreateFormPreview.tsx
+++ src/components/CreateFormPreview.tsx
@@ -2,6 +2,7 @@ import { Header } from "./Header";
 import styled from "@emotion/styled";
 import { Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useFormStore } from "../store";
 
 const Heading = styled.h1`
   margin: 1.5rem 0;
@@ -9,15 +10,19 @@ const Heading = styled.h1`
   font-weight: 500;
 `;
 
-export const CreateFormPreview = () => (
-  <>
-    <Header
-      leftContent={<Heading>Create Form Preview</Heading>}
-      rightContent={
-        <Link to="/create-form">
-          <Button size="sm">Edit</Button>
-        </Link>
-      }
-    />
-  </>
-);
+export const CreateFormPreview = () => {
+  const form = useFormStore((state) => state.form);
+  return (
+    <>
+      <Header
+        leftContent={<Heading>Create Form Preview</Heading>}
+        rightContent={
+          <Link to="/create-form">
+            <Button size="sm">Edit</Button>
+          </Link>
+        }
+      />
+      {JSON.stringify(form)}
+    </>
+  );
+};
